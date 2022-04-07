a35f2acf2d61a735b7dd7c8fbbb49f175b752aeb
FajarRamadhan27 <fajarramadhan220@gmail.com>
2022-04-08 04:35:42 +0700

    refactor table

--- resources/js/components/pages/sidebar_menus/EmployeMasterData.js
+++ resources/js/components/pages/sidebar_menus/EmployeMasterData.js
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material"
-import EmployeeTable from "../../containers/EmployeeTable"
+import EmployeeTable from "../../containers/tables/EmployeeTable"
 
 function EmployeeMasterData() {
     return (
